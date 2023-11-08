bf911025c2ff59ff153ad9cab31075aa503126b0
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-08 19:39:21 -0300

    Commit inicial
    Commit inicial

---
+++ 12-APIVersioning/Exemplo/V1/Models/ResultadoContador.cs
@@ -0,0 +1,10 @@
+namespace Exemplo.V1.Models;
+
+public class ResultadoContador
+{
+    public int ValorAtual { get; set; }
+    public string? Local { get; set; }
+    public string? Kernel { get; set; }
+    public string? Framework { get; set; }
+    public string? Mensagem { get; set; }
+}
---
+++ 12-APIVersioning/Exemplo/V2/Controllers/ContadorController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Exemplo.V2.Models;
+
+namespace Exemplo.V2.Controllers;
+
+[ApiController]
+[ApiVersion("2.0")]
+[Route("[controller]")]
+[Route("v{version:apiVersion}/[controller]")]
+public class ContadorController : ControllerBase
+{
+    private static readonly Contador _CONTADOR = new Contador();
+    private readonly ILogger<ContadorController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public ContadorController(ILogger<ContadorController> logger,
+        IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public ResultadoContador GetV2_0()
+    {
+        int valorAtualContador;
+        lock (_CONTADOR)
+        {
+            _CONTADOR.Incrementar();
+            valorAtualContador = _CONTADOR.ValorAtual;
+        }
+
+        _logger.LogInformation($"Contador - Valor atual: {valorAtualContador}");
+
+        var resultado = new ResultadoContador()
+        {
+            ValorAtual = valorAtualContador,
+            Versao = "2.0",
+            Local = _CONTADOR.Local,
+            Kernel = _CONTADOR.Kernel,
+            Framework = _CONTADOR.Framework,
+            Mensagem = "Versao 2.0"
+        };
+
+        return resultado;
+    }
+}
